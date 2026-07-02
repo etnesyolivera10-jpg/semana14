089fbb050e3a23187a0ef071623c612593e5bd9e
Etnesy <etnesyolivera10@gmail.com>
2026-07-02 02:51:13 -0500

    version1

---
+++ Ejercicio 4/Ejercicio 4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    internal class Program
+    {
+
+            static string NormalizarCodigo(string codigo)
+            {
+                codigo = codigo.Trim();
+
+                codigo = codigo.Replace("-", "");
+
+                codigo = codigo.ToUpper();
+
+                return codigo;
+            }
+
+            static bool TieneLongitudValida(string codigo)
+            {
+                return codigo.Length == 8;
+            }
+
+            static void Main()
+            {
+                Console.Write("Ingrese código de comprobante: ");
+
+                string codigoIngresado = Console.ReadLine();
+
+                string codigoNormalizado = NormalizarCodigo(codigoIngresado);
+
+                Console.WriteLine($"Código normalizado: {codigoNormalizado}");
+
+                if (TieneLongitudValida(codigoNormalizado))
+                {
+                    Console.WriteLine("Código válido.");
+                }
+                else
+                {
+                    Console.WriteLine("Código inválido. Debe tener 8 caracteres.");
+                }
+            }
+
+        }
+
+    }
+
---
+++ Ejercicio 4/ejercicio5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio5
+{
+    internal class Program
+    {
+        static int BuscarEstudiante(string[] nombres, string texto)
+        {
+            texto = texto.Trim().ToLower();
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                if (nombres[i].ToLower().Contains(texto))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        static void Main()
+        {
+            string[] nombres = { "Etnesy Olivera", "Maria Lopez", "Andrea Diaz", "Ana Torres" };
+            double[] notas = { 18.5, 15.0, 17.2, 19.0 };
+
+            Console.Write("Ingrese parte del nombre del estudiante: ");
+            string texto = Console.ReadLine();
+
+            int posicion = BuscarEstudiante(nombres, texto);
+
+            if (posicion != -1)
+            {
+                Console.WriteLine("Estudiante: " + nombres[posicion]);
+                Console.WriteLine("Nota: " + notas[posicion]);
+            }
+            else
+            {
+                Console.WriteLine("Estudiante no encontrado.");
+            }
+        }
+    }
+}
