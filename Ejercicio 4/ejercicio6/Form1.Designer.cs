562a452e83e699d7cd5793cedb01c1518e771764
Etnesy <etnesyolivera10@gmail.com>
2026-07-03 20:12:43 -0500

    version3

--- Ejercicio 4/ejercicio6/Form1.Designer.cs
+++ Ejercicio 4/ejercicio6/Form1.Designer.cs
@@ -37,22 +37,25 @@
             // 
             // lblTituloRanking
             // 
-            this.lblTituloRanking.AutoSize = true;
-            this.lblTituloRanking.Location = new System.Drawing.Point(305, 53);
+            this.lblTituloRanking.BackColor = System.Drawing.Color.RoyalBlue;
+            this.lblTituloRanking.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloRanking.Location = new System.Drawing.Point(79, 31);
             this.lblTituloRanking.Name = "lblTituloRanking";
-            this.lblTituloRanking.Size = new System.Drawing.Size(161, 13);
+            this.lblTituloRanking.Size = new System.Drawing.Size(599, 36);
             this.lblTituloRanking.TabIndex = 0;
             this.lblTituloRanking.Text = "Ranking de estudiantes por nota";
+            this.lblTituloRanking.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnOrdenar
             // 
             this.btnOrdenar.AccessibleName = "btnOrdenar";
+            this.btnOrdenar.BackColor = System.Drawing.Color.Gold;
             this.btnOrdenar.Location = new System.Drawing.Point(243, 97);
             this.btnOrdenar.Name = "btnOrdenar";
             this.btnOrdenar.Size = new System.Drawing.Size(114, 36);
             this.btnOrdenar.TabIndex = 1;
             this.btnOrdenar.Text = "Ordenar ranking";
-            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.UseVisualStyleBackColor = false;
             this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
             // 
             // btnRestablecerRanking
@@ -78,18 +81,19 @@
             // lblMayor
             // 
             this.lblMayor.AccessibleName = "lblMayor";
-            this.lblMayor.AutoSize = true;
+            this.lblMayor.BackColor = System.Drawing.Color.LemonChiffon;
             this.lblMayor.Location = new System.Drawing.Point(76, 356);
             this.lblMayor.Name = "lblMayor";
-            this.lblMayor.Size = new System.Drawing.Size(80, 13);
+            this.lblMayor.Size = new System.Drawing.Size(602, 37);
             this.lblMayor.TabIndex = 4;
             this.lblMayor.Text = "Primer puesto: -";
+            this.lblMayor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
-            // Form
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lblMayor);
             this.Controls.Add(this.lstRanking);
@@ -99,7 +103,6 @@
             this.Name = "Form1";
             this.Text = "|";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
